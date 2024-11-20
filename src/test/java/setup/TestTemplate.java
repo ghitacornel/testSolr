8e7079e10e67fa46b2e5f92a3ce9d4f78308e7a5
cornel <ghita.cornel@gmail.com>
2024-11-20 18:28:11 +0200

    better

--- src/test/java/setup/TestTemplate.java
+++ src/test/java/setup/TestTemplate.java
@@ -2,7 +2,7 @@ package setup;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.impl.Http2SolrClient;
 import org.apache.solr.client.solrj.impl.XMLResponseParser;
 import org.junit.After;
 import org.junit.Before;
@@ -33,30 +33,30 @@ public abstract class TestTemplate {
         zkServers.add("localhost:2181");
         zkServers.add("localhost:2182");
         zkServers.add("localhost:2183");
-        CloudSolrClient client = new CloudSolrClient.Builder(zkServers, Optional.empty()).build();
-        client.setDefaultCollection(COLLECTION_NAME);
-        return client;
+        return new CloudSolrClient.Builder(zkServers, Optional.empty())
+                .withDefaultCollection(COLLECTION_NAME)
+                .build();
     }
 
     private SolrClient getHttpSolrClient1() {
         String urlString = "http://localhost:8981/solr/" + COLLECTION_NAME;
-        HttpSolrClient client = new HttpSolrClient.Builder(urlString).build();
-        client.setParser(new XMLResponseParser());
-        return client;
+        return new Http2SolrClient.Builder(urlString)
+                .withResponseParser(new XMLResponseParser())
+                .build();
     }
 
     private SolrClient getHttpSolrClient2() {
         String urlString = "http://localhost:8982/solr/" + COLLECTION_NAME;
-        HttpSolrClient client = new HttpSolrClient.Builder(urlString).build();
-        client.setParser(new XMLResponseParser());
-        return client;
+        return new Http2SolrClient.Builder(urlString)
+                .withResponseParser(new XMLResponseParser())
+                .build();
     }
 
     private SolrClient getHttpSolrClient3() {
         String urlString = "http://localhost:8983/solr/" + COLLECTION_NAME;
-        HttpSolrClient client = new HttpSolrClient.Builder(urlString).build();
-        client.setParser(new XMLResponseParser());
-        return client;
+        return new Http2SolrClient.Builder(urlString)
+                .withResponseParser(new XMLResponseParser())
+                .build();
     }
 
     private SolrClient getCloudSolrClientCluster() {
@@ -64,9 +64,9 @@ public abstract class TestTemplate {
         zkServers.add("localhost:2181");
         zkServers.add("localhost:2182");
         zkServers.add("localhost:2183");
-        CloudSolrClient client = new CloudSolrClient.Builder(zkServers, Optional.empty()).build();
-        client.setDefaultCollection(COLLECTION_NAME);
-        return client;
+        return new CloudSolrClient.Builder(zkServers, Optional.empty())
+                .withDefaultCollection(COLLECTION_NAME)
+                .build();
     }
 
     @Before
